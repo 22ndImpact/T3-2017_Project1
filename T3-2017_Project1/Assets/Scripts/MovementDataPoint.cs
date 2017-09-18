9ec6389dd3b95f69868efb94ad2ceb0b3d0476e3
DLong94 <dlong03111994@gmail.com>
2017-09-18 13:32:54 +1000

    Added MovementDataPoint class

---
+++ T3-2017_Project1/Assets/Scripts/MovementDataPoint.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementDataPoint
+{
+    public Vector3 position;
+    public Vector3 direction;
+    public float timestamp;
+
+    //Constructor
+    public MovementDataPoint(Vector3 pos, Vector3 dir, float time)
+    {
+        this.position = pos;
+        this.direction = dir;
+        this.timestamp = time;
+    }
+}
+
+public class MovementDataList
+{
+    public List<MovementDataPoint> dataList;
+    public int versionNumber;
+
+    //Constructor
+    public MovementDataList(int version)
+    {
+        this.dataList = new List<MovementDataPoint>();
+        this.versionNumber = version;
+    }
+
+    //Constructor
+    public MovementDataList(int version, List<MovementDataPoint> list)
+    {
+        this.dataList = list;
+        this.versionNumber = version;
+    }
+}
