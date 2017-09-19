58ae7bb10c36b95ae33e0fe29b1dfa0a49289bc1
Mitchell Pryor <Mitchell@hopskipfall.com>
2017-09-20 08:04:39 +1000

    - Brute force commit of currently up to date project files - Ready to be transfered over to Lanz's repo

---
+++ T3-2017_Project1/Assets/Scripts/MovementDataPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementDataPoint
+{
+    public Vector3 position;
+    public Vector3 direction;
+    public float timestamp;
+
+    //Constructor
+    public MovementDataPoint(Vector3 pos, Vector3 dir, float time)
+    {
+        this.position = pos;
+        this.direction = dir;
+        this.timestamp = time;
+    }
+}
